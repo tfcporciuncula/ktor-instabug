1d4830927fdbffd73fe8c9dec4b07ab84dcd7be5
Fred Porciúncula <tfcporciuncula@gmail.com>
2023-10-12 18:07:03 +0200

    Add Instabug on iOS
    The token there is from a test project.

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,15 @@
 import SwiftUI
+import Instabug
 
 @main
 struct iOSApp: App {
+
+  init() {
+//    Instabug.disableMethodSwizzling() // Everything works fine when this is uncommented
+    Instabug.start(withToken: "dac439f0528a9b846955749816bce619", invocationEvents: .shake)
+    Instabug.welcomeMessageMode = .disabled
+  }
+
 	var body: some Scene {
 		WindowGroup {
 			ContentView()
