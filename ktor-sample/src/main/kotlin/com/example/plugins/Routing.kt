70da86a4cb04d7ae2b2d3d06b12f10c58f5ec9b0
Fred Porciúncula <tfcporciuncula@gmail.com>
2023-10-12 15:55:56 +0200

    Add Ktor server sample
    Built from start.ktor.io with the addition of an endpoint with form data based on https://ktor.io/docs/requests.html#form_data

---
+++ ktor-sample/src/main/kotlin/com/example/plugins/Routing.kt
@@ -0,0 +1,41 @@
+package com.example.plugins
+
+import io.ktor.http.content.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import java.io.File
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+
+        var fileDescription = ""
+        var fileName = ""
+
+        post("/upload") {
+            val multipartData = call.receiveMultipart()
+
+            multipartData.forEachPart { part ->
+                when (part) {
+                    is PartData.FormItem -> {
+                        fileDescription = part.value
+                    }
+
+                    is PartData.FileItem -> {
+                        fileName = part.originalFileName as String
+                        println("File received: $fileName")
+                    }
+
+                    else -> {}
+                }
+                part.dispose()
+            }
+
+            call.respondText("$fileDescription is uploaded to 'uploads/$fileName'")
+        }
+    }
+}
